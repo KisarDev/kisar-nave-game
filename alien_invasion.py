ee2056a960847e078e61aba0900bf02b8d4bf0d1
unity-team-developers <cesarmartins.pro@gmail.com>
2024-01-30 13:51:00 +0000

    :sparkles: feat: bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import game_functions as gf
 from settings import Settings
 from ship import Ship
 import pygame
+from pygame.sprite import Group
 
 
 def run_game():
@@ -15,14 +16,16 @@ def run_game():
     # troca a cor da tela em cada frame
     screen.fill(ai_settings.bg_color)
     ship = Ship(ai_settings, screen)
+    bullets = Group()
 
     # Inicia o laço principal do jogo
     while True:
         ship.update()
+        bullets.update()
         # Observa eventos de teclado e de mouse
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         # Deixa a tela mais recente visível
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,26 +1,32 @@
 import sys
 import pygame
+from bullet import Bullet
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Responde a eventos de pressionamento de teclas e de mouse."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
 
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Responde a pressionamentos de tecla."""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
 
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+
+    elif event.key == pygame.K_SPACE:
+        # Cria um novo projétil e o adiciona
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -32,9 +38,12 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Atualiza as imagens na
     tela e alterna para a nova tela."""
     screen.fill(ai_settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
+    # Redesenha todos os projéteis atrás da espaçonave e dos alienígenas y
     pygame.display.flip()
