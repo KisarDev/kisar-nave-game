d6d9d7fd253f8e51d0d50b5ae49c320420f597f7
unity-team-developers <cesarmartins.pro@gmail.com>
2024-01-30 13:08:20 +0000

    :recycle: movemment & limit border

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,12 +14,13 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     # troca a cor da tela em cada frame
     screen.fill(ai_settings.bg_color)
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # Inicia o laço principal do jogo
     while True:
+        ship.update()
         # Observa eventos de teclado e de mouse
-        gf.check_events()
+        gf.check_events(ship)
         # Deixa a tela mais recente visível
         gf.update_screen(ai_settings, screen, ship)
 
